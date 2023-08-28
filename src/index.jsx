90110534cb95c58324e935fae723fbb73199ca10
rorymcd98 <rorymcd98@gmail.com>
2023-08-28 17:48:43 +0100

    configured amplify

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "../css/App.css";
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import { Amplify } from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
